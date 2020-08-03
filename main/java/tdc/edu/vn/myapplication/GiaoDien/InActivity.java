1e36629adb25148a0884abbeef047d32fa9b5a73
= <=>
2020-08-03 08:39:23 +0700

    chac nang 2

--- main/java/tdc/edu/vn/myapplication/GiaoDien/InActivity.java
+++
@@ -1,9 +0,0 @@
-package tdc.edu.vn.myapplication.GiaoDien;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class InActivity extends AppCompatActivity {
-
-
-}
